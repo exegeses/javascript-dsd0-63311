29bce417a2b750579cf74617af45f48360e7a8e4
Marcos <exegeses@gmail.com>
2023-04-05 14:45:53 -0300

    resolución tarea mostrar|ocultar usando funciones

--- clase4/js/mostrar-ocultar.js
+++ clase4/js/mostrar-ocultar.js
@@ -1,23 +1,27 @@
 //ubicamos elemento dentro del DOM
 const caja = document.querySelector('#caja');
-
+const boton = document.querySelector('#boton');
 
 //declaramos funciones de control
 function ocultarCaja()
 {
     caja.style.display = 'none';
+    boton.innerText = 'Mostrar';
 }
 function mostrarCaja()
 {
     caja.style.display = 'block';
+    boton.innerText = 'Ocultar';
 }
 
 function mostrarOcultar()
 {
     if( caja.style.display == 'none' ){
         mostrarCaja();
+        /* boton.innerText = 'Ocultar';*/
     }
     else{
         ocultarCaja();
+        /* boton.innerText = 'Mostrar'; */
     }
 }
